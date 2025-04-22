6c3482aa07c6be16c14138e10a50b023f65bf731
cat <hello@cat.cn>
2025-04-22 14:35:59 +0800

    添加工具类

---
+++ src/main/java/com/yupi/project/Utils/StringToUpper.java
@@ -0,0 +1,11 @@
+package com.yupi.project.Utils;
+
+public class StringToUpper {
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String upperCase = original.toUpperCase();
+        
+        System.out.println("原始字符串: " + original);
+        System.out.println("转换后字符串: " + upperCase);
+    }
+}
